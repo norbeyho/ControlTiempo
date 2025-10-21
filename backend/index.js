43e9890e9db5b1d0a885817870f465eafd5ac2e8
exgisa <exgisaparqueadero@gmail.com>
2025-10-21 11:08:26 -0500

    Modificar indexjs

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 import dotenv from "dotenv";
 import app from "./src/app.js";
+import serverless from "serverless-http"
+
+export const handler = serverless(app);
 
 dotenv.config();
 console.log("Mongo URi",process.env.MONGODB_URI)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", ()=> {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+// app.listen(PORT, "0.0.0.0", ()=> {
+//     console.log(`Servidor corriendo en el puerto ${PORT}`);
+// });
 
 app.get('/', (req, res) => {
     res.send("Api funcionando")
